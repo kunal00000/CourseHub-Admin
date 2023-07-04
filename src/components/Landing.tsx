6a4c4d518d6893fdd1c5d681fb5ccfe37961b26c
kunal00000 <kunalverma2468@gmail.com>
2023-07-05 00:49:11 +0530

    Basic Landing Page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,14 +1,14 @@
 /// This is the landing page. You need to add a link to the login page here.
 /// Maybe also check from the backend if the user is already logged in and then show them a logout button
 /// Logging a user out is as simple as deleting the token from the local storage.
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Text, Image, Center } from "@mantine/core";
 
 const Landing = () => {
   return (
-    <div className="w-[100vw]">
+    <div className="w-[100vw] h-[100vh]">
       <nav>
         <Group position="apart" className="m-10 mx-24">
-          <span className="text-3xl font-bold">Graphy</span>
+          <Text className="text-3xl font-bold">CourseHub</Text>
           <Group>
             <a href="/login">
               <Button variant="subtle" color="cyan">
@@ -23,8 +23,42 @@ const Landing = () => {
           </Group>
         </Group>
       </nav>
-      <h1>Welcome to course selling website!</h1>
-      <br />
+      <Center h={"65vh"}>
+        <Group position="apart" className="w-[85vw]">
+          <div>
+            <Text fw={700} lineClamp={2} className="text-5xl text-bold">
+              Build supercharged <br />
+              online courses
+            </Text>
+            <Text className="my-7">
+              Create custom courses, build beautiful no-code websites, or launch
+              <br />
+              your own app — all using just CourseHub.
+            </Text>
+            <Group className="" spacing="sm">
+              <a href="/register">
+                <Button variant="outline" color="teal">
+                  Start for free
+                </Button>
+              </a>
+              <a href="/register">
+                <Button variant="default">Book a demo</Button>
+              </a>
+            </Group>
+          </div>
+          <div>
+            <Image
+              width={540}
+              height={490}
+              fit="contain"
+              src={"https://graphy.com/images/herofeature1.png"}
+              alt="CourseHub"
+              caption="Over 100K+ creators have launched their online
+              teaching businesses using CourseHub."
+            />
+          </div>
+        </Group>
+      </Center>
     </div>
   );
 };
