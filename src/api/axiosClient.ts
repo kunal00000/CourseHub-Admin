bbb10f8c4b9adf8bd71148e4d47eb3cb6423e56e
kunal00000 <kunalverma2468@gmail.com>
2023-07-05 02:16:36 +0530

    refactor: instance type corrections

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
-const axiosClient = axios.create({
+const axiosClient: AxiosInstance = axios.create({
   baseURL: "http://localhost:3000/",
   headers: {
     "Content-Type": "application/json",
