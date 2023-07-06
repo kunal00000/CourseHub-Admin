07766c980ef8d11587f2427b5b64f40910c1e714
kunal00000 <kunalverma2468@gmail.com>
2023-07-07 00:17:12 +0530

    feat: Create Course feature implemented

--- src/components/course/CreateCourse.tsx
+++ src/components/course/CreateCourse.tsx
@@ -1,14 +1,106 @@
-import React from "react";
-/// You need to add input boxes to take input for users to create a course.
-/// I've added one input so you understand the api to do it.
+import {
+  Card,
+  TextInput,
+  Text,
+  Button,
+  NumberInput,
+  Group,
+  Select,
+  Textarea,
+} from "@mantine/core";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useCoursePostMutation } from "../../hooks/useCourse";
+
 function CreateCourse() {
-  const [title, setTitle] = React.useState("");
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [imageLink, setLink] = useState("");
+  const [price, setPrice] = useState<number | "">(0);
+  const [published, setPublished] = useState<string | null>("true");
+
+  const { mutate } = useCoursePostMutation();
+  function onCreateCourse() {
+    const data = {
+      title: title,
+      description: desc,
+      imageLink: imageLink,
+      price: price,
+      published: published === "true" ? true : false,
+    };
+    console.log(data);
+    mutate(data);
+  }
 
   return (
     <div>
-      <h1>Create Course Page</h1>
-      <input type={"text"} onChange={(e) => setTitle(e.target.value)} />
-      <button onClick={() => console.log(title)}>Create Course</button>
+      <Card p={"xl"} w={"50vw"} m={"auto"} shadow="lg">
+        <Group position="center">
+          <Text size={"xl"} color="teal" weight={"bold"}>
+            Create Course Page
+          </Text>
+        </Group>
+        <TextInput
+          label="Title"
+          placeholder="Learn to be a pro..."
+          withAsterisk
+          p={"xs"}
+          onChange={(e) => {
+            setTitle(e.currentTarget.value);
+          }}
+        />
+        <Textarea
+          label="Description"
+          placeholder="Get to know these 5 techniques..."
+          withAsterisk
+          autosize
+          minRows={2}
+          maxRows={4}
+          p={"xs"}
+          onChange={(e) => {
+            setDesc(e.currentTarget.value);
+          }}
+        />
+        <TextInput
+          label="Image Link"
+          placeholder="image link here..."
+          withAsterisk
+          p={"xs"}
+          onChange={(e) => {
+            setLink(e.currentTarget.value);
+          }}
+        />
+        <Group position="apart">
+          <NumberInput
+            label="Price (in $)"
+            placeholder="best price you can offer..."
+            withAsterisk
+            value={price}
+            min={0}
+            w={"50%"}
+            p={"xs"}
+            onChange={setPrice}
+          />
+          <Select
+            label="Publish"
+            data={[
+              { value: "true", label: "Publish Now" },
+              { value: "false", label: "Publish Later" },
+            ]}
+            defaultValue={published}
+            p={"xs"}
+            onChange={setPublished}
+          />
+        </Group>
+        <Group p={"sm"} position="right">
+          <Link to={"/dashboard/courses"} replace={true}>
+            <Button variant="default">Cancel</Button>
+          </Link>
+          <Button variant="outline" onClick={onCreateCourse}>
+            Create Course
+          </Button>
+        </Group>
+      </Card>
     </div>
   );
 }
